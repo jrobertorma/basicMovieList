9ecfa5de0af203fe10c23c4b7b9669ecca113601
jrobertorma <jrobertorma@gmail.com>
2021-06-22 08:25:23 -0500

    Configuring webpack and react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("yoyoyo, soy movieList index.js");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList yoyoyo</h1>
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
