9ef3f3a9e900b7645f5377a27c15656f3b7bec68
jrobertorma <jrobertorma@gmail.com>
2021-06-28 11:30:46 -0500

    Adding search functionality

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const MovieCard = ({movie}) => {
-    return ( <p>Soy Movie Card: {movie.Title}</p> );
+    return ( 
+        <div>  
+            <p>Title: {movie.Title}</p>
+            <p>Year: {movie.Year}</p>
+            <p>imdbID: {movie.imdbID}</p>
+            <p>Type: {movie.Type}</p>
+            <p>Poster: {movie.Poster}</p>
+        </div>
+    );
 }
  
 export default MovieCard;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import MovieList from './containers/MovieList';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
 
-	const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?s=Batman&apikey=25b028b4`;
+	const getMovieRequest = async (searchValue) => {
+		const url = searchValue 
+			? `http://www.omdbapi.com/?s=${searchValue}&apikey=25b028b4`
+			: `http://www.omdbapi.com/?s=&apikey=25b028b4`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -17,14 +20,31 @@ const App = () => {
 		}
 	};
 
+	const handleSearchSubmit = (event) => {
+		getMovieRequest(searchTerm);
+		event.preventDefault();
+	};
+
 	useEffect(() => {
 		getMovieRequest();
 	}, []);
 
     return (
         <div>
-            <h1>movieList yoyoyo root component</h1>
-            <p>{console.log(movies[0])}</p>
+            <h1>movieList</h1>
+			
+			<form onSubmit={ (event) => handleSearchSubmit(event) }>
+				<input 
+					type="text" 
+					name="searchTerm" 
+					value={searchTerm}
+					onChange={ (event) => setSearchTerm(event.target.value) }
+				/>
+				<button type="submit">
+					Search
+				</button>
+			</form>
+
             <MovieList movies={movies} />
         </div>
     )
