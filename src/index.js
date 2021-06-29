e095271b733eebbc9f864aebd44b487aa421e88d
jrobertorma <jrobertorma@gmail.com>
2021-06-29 08:24:05 -0500

    Adding pagination

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import MovieList from './containers/MovieList';
 const App = () => {
     const [movies, setMovies] = useState([]);
 	const [searchTerm, setSearchTerm] = useState('');
+	let [pageNumber, setPageNumber] = useState(1);
 
-	const getMovieRequest = async (searchValue) => {
-		const url = searchValue 
-			? `http://www.omdbapi.com/?s=${searchValue}&apikey=25b028b4`
+	const getMovieRequest = async (searchTerm) => {
+		const url = searchTerm 
+			? `http://www.omdbapi.com/?s=${searchTerm}&apikey=25b028b4&page=${pageNumber}`
 			: `http://www.omdbapi.com/?s=&apikey=25b028b4`;
-
+		console.log(url);
 		const response = await fetch(url);
 		const responseJson = await response.json();
 
@@ -25,9 +26,20 @@ const App = () => {
 		event.preventDefault();
 	};
 
+	const previousPage = () => {
+		pageNumber > 1 ? setPageNumber(pageNumber - 1): pageNumber;
+		getMovieRequest(searchTerm);
+	}
+
+	const nextPage = () => {
+		setPageNumber(pageNumber + 1);
+		
+		getMovieRequest(searchTerm);
+	}
+
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchTerm);
+	}, [searchTerm]);
 
     return (
         <div>
@@ -44,7 +56,16 @@ const App = () => {
 					Search
 				</button>
 			</form>
-
+			
+			<div>
+				<p>Page:{pageNumber}</p>
+				<button onClick={() => previousPage()}>
+					Previous page
+				</button>
+				<button onClick={() => nextPage()}>
+					Next Page
+				</button>
+			</div>
             <MovieList movies={movies} />
         </div>
     )
