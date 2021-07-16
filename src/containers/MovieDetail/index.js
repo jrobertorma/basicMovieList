8d64854cb9c6793fa3f740a19d71d93d1513b7a1
jrobertorma <jrobertorma@gmail.com>
2021-07-16 09:08:15 -0500

    Redirecting components

--- src/containers/MovieDetail/index.js
+++ src/containers/MovieDetail/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import { Link as RouterLink } from 'react-router-dom';
 
 const MovieDetail = () => {
     let { id } = useParams();
 
     return (
         <div>
+            <Button size="small" color="primary" component={RouterLink} to={"/"}>
+                Back
+            </Button>
             <h3>ID: {id}</h3>
             <p>Soy movie detail</p>
         </div>
@@ -13,3 +18,7 @@ const MovieDetail = () => {
 }
 
 export default MovieDetail;
+
+/**
+ * https://material-ui.com/components/modal/#transitions
+ */
